87dd0ed10519b6268f2e5bc4419b01ce03109ca4
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2018-11-08 02:11:05 +0600

    Update taskA.php

--- taskA.php
+++ taskA.php
@@ -12,7 +12,7 @@ class Student
 {
 	public $name;
 	public $balance=50000;
-	public $courlist=array();
+	public $courlist= [];
 	function __construct($name)
 	{
 		$this->name=$name;
@@ -21,17 +21,17 @@ class Student
 	{
 		return $this->courlist;
 	}
-	public function addcourse($course)
+	public function addcourse($Course)
 	{
-		array_push($this->courlist,$course);
-		$balance=$balance-10000;
+		array_push($this->courlist,$Course->name);
+        $this->balance-10000;
 	}
 	public function deletecourse($course)
 	{
 		if (($key = array_search($course, $array)) !== false) 
 		{
 		unset($array[$key]);
-		$ballance=$ballance+10000;
+		$this->balance+10000;
 		}
 	}
 	public function getballance()
@@ -44,14 +44,14 @@ $studentTwo=new Student('studentTwo');
 $studentThree=new Student('studentThree');
 
 
-$php=new Course('php');
+$c1=new Course('php');
 $csharp=new Course('csharp');
 $cplusplus=new Course('cplusplus');
 
 echo $studentOne->getballance().'studentOne'.'before any course'.'\n';
 echo $studentTwo->getballance().'studentTwo'.'before any course'.'\n';
 echo $studentThree->getballance().'studentThree'.'before any course'.'\n';
-$studentOne->addcourse('php');
+$studentOne->addcourse($c1);
 echo $studentOne->getcourse();
 
 
